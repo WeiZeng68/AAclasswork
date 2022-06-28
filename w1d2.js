5f4ef1403546d2aaa0e3870ce496f1d718b112e7
WeiZeng68 <weilouzeng@gmail.com>
2022-06-28 19:09:20 -0400

    d2 work

---
+++ w1d2.js
@@ -0,0 +1,35 @@
+// function to check if num is odd using true false
+// function isOdd(num) {
+//     return num % 2 !== 0; 
+// }
+
+// console.log(isOdd(9));
+
+function isOdd(num) {
+    if (num % 2 !== 0)
+    return true;
+    else
+    return false; 
+}
+
+console.log(isOdd(9));
+// //
+// function plusFive(num) {
+//     return num + 5;
+//   }
+
+// console.log(plusFive(5));
+
+
+// function threeOrSeven(num) {
+//     return num % 7 === 0 || num % 3 === 0;
+// }
+
+// console.log(threeOrSeven(20));
+
+// function hello(str) {
+//     return ("hello" + str);
+//   }
+
+// console.log(hello(" the crabs are delicious today"));
+
