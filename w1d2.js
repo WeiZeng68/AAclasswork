319db809d22bf9b95c8a2dce15b660d465fcfc43
WeiZeng68 <weilouzeng@gmail.com>
2022-06-28 19:37:23 -0400

    day2work

--- w1d2.js
+++ w1d2.js
@@ -1,18 +1,18 @@
 // function to check if num is odd using true false
 // function isOdd(num) {
-//     return num % 2 !== 0; 
+//  return num % 2 !== 0; 
 // }
 
 // console.log(isOdd(9));
 
-function isOdd(num) {
+ function isOdd(num) {
     if (num % 2 !== 0)
-    return true;
-    else
-    return false; 
-}
+   return true;
+  else
+  return false; 
+ }
 
-console.log(isOdd(9));
+// console.log(isOdd(9));
 // //
 // function plusFive(num) {
 //     return num + 5;
@@ -33,3 +33,27 @@ console.log(isOdd(9));
 
 // console.log(hello(" the crabs are delicious today"));
 
+// function yell(string) {
+//     return string.toUpperCase();
+//   }
+
+// console.log(yell("the soup sucks today"));
+
+// function whisper(string) {
+//     return string.toLowerCase();
+//   }
+
+// console.log("THE SOUP IS GOOD TODAY");
+
+// function isSubstring(searchString, subString) {
+     
+//   }
+
+
+function averageOfFour(num1, num2, num3, num4){
+    let sum = num1 + num2 + num3 + num4;
+    let avg = sum / 4;
+    return avg;
+}
+
+console.log(averageOfFour(1,2,3,4));
