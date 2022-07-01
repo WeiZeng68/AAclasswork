f0d52cdf85cdf58ed422b4522c115d9b470c33e9
WeiZeng68 <weilouzeng@gmail.com>
2022-06-30 20:20:48 -0400

    day4

--- w1d4.js
+++ w1d4.js
@@ -13,7 +13,7 @@
 //         console.log(arr[i]);
 //     }
 // }
-let myPeople = ['Volkanovski', 'Adesanya', 'Cannonier', 'Holloway'];
+// let myPeople = ['Volkanovski', 'Adesanya', 'Cannonier', 'Holloway'];
 // console.log(printArray(myPeople));
 
 //function to print array in reverse
@@ -27,10 +27,37 @@ let myPeople = ['Volkanovski', 'Adesanya', 'Cannonier', 'Holloway'];
 
 // Write a function `logEach(array)` that prints every element of the array and its
 // index to the console.
-function logEach(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log('index: ' + i + ' element: ' + array[i]);
+// function logEach(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log('index: ' + i + ' element: ' + array[i]);
+//     }
+// }
+
+// console.log(logEach(myPeople));
+
+// Write a function sumArray(array) that takes in an array of numbers that returns the
+// total sum of them.
+// function sumArray(array) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         let num = array[i];
+//         sum += num;
+//     }
+// return console.log(sum);
+// }
+
+// sumArray([5, 6, 4]); // => 15
+// sumArray([7, 3, 9, 11]); // => 30
+
+
+// Write a function range(start, end) that returns an array that contains all
+// numbers between 'start' and 'end' in sequential order.
+function Range(start, end) {
+    let array = [];
+    for (i = start; i < array.length; i++) {
     }
+    return array[i];
 }
 
-console.log(logEach(myPeople));
+console.log(Range(1,4));
+console.log(Range(4,2));
